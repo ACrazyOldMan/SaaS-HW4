9664ffbb1b9acbb0b589831a7ef16d7c8d800c73
ACrazyOldMan <ACrazyOldMan@gmail.com>
2012-03-28 02:48:25 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rottenpotatoes::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+# The priority is based upon order of creation:
+# first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+# Sample of regular route:
+#   match 'products/:id' => 'catalog#view'
+# Keep in mind you can assign values other than :controller and :action
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+# Sample of named route:
+#   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+# This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+# Sample resource route (maps HTTP verbs to controller actions automatically):
+#   resources :products
   resources :movies
 
   # Sample resource route with options:
@@ -56,4 +56,11 @@ Rottenpotatoes::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  resource :movies do
+    collection do
+      get 'similar'
+    end
+  end
+
 end
